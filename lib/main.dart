2de3c366b0353c70a7638ff18c67d144d8977ead
Emir Halilović <66942813+EmirYNWA@users.noreply.github.com>
2024-05-23 13:38:22 +0200

    Shown the generated text on the screen and allowed user to select it and copy it using gesture detector. Fixed the application design a bit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -33,6 +34,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   File? _imageFile;
+  String generatedText = "";
 
   Future<void> _pickImage() async {
     final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
@@ -49,7 +51,9 @@ class _MyHomePageState extends State<MyHomePage> {
     final textRecognizer = TextRecognizer();
     final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
     String extractedText = recognizedText.text;
-    print(extractedText);
+    setState(() {
+      generatedText = extractedText;
+    });
   }
 
   @override
@@ -68,12 +72,32 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               _imageFile == null
                   ? const Text('Select an image to analyze')
-                  : Image.file(_imageFile!),
+                  : Container(
+                      padding: const EdgeInsets.only(left: 40, right: 40),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.file(_imageFile!),
+                      ),
+                  ),
               const SizedBox(height: 20,),
               ElevatedButton(
-                  onPressed: _pickImage,
-                  child: const Text('Pick Image'),
-              )
+                onPressed: _pickImage,
+                child: const Text('Pick Image'),
+              ),
+              const SizedBox(height: 20,),
+              SelectableText(
+                generatedText,
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(height: 20,),
+              GestureDetector(
+                child: const Icon(Icons.copy, size: 36),
+                onTap: () {
+                  Clipboard.setData(ClipboardData(text: generatedText));
+                },
+              ),
+              const SizedBox(height: 20,),
             ],
           ),
         ),
